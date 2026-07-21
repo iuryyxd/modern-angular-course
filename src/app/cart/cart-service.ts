3f99a53b9953c8e9d6e628e1f1ee1b97c62c9224
Iury <iurysena74@gmail.com>
2026-07-21 19:46:21 -0300

    feat: add cart badge quantity

--- src/app/cart/cart-service.ts
+++ src/app/cart/cart-service.ts
@@ -1,14 +1,30 @@
-import { Injectable, signal } from '@angular/core';
+import { computed, Injectable, signal } from '@angular/core';
 import { Product } from '../products/product';
+import { CartItem } from './cart-item';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CartService {
-  private readonly cartItems = signal<Product[]>([]);
+  private readonly cartItems = signal<CartItem[]>([]);
+  readonly totalItems = computed(() =>
+    this.cartItems().reduce((total, item) => total + item.quantity, 0),
+  );
 
   addToCart(product: Product) {
-    console.log('Product added to cart:', product);
-    this.cartItems.update((items) => [...items, product]);
+    this.cartItems.update((items) => {
+      const existingItem = items.find((item) => item.product.id === product.id);
+      if (existingItem) {
+        return items.map((item) => {
+          if (item.product.id === product.id) {
+            return { ...item, quantity: item.quantity + 1 };
+          }
+
+          return item;
+        });
+      }
+
+      return [...items, { product, quantity: 1 }];
+    });
   }
 }
--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
+import { CartService } from '../cart/cart-service';
 
 @Component({
   selector: 'app-header',
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatBadgeModule],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
-export class Header {}
+export class Header {
+  protected readonly cartService = inject(CartService);
+}
